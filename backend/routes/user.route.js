d19d1185426300fc42728c7872eedcc3f104be97
iDinesh04 <dineshkumarsahoo.2025@gmail.com>
2024-12-30 23:51:31 +0530

    some update

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,11 +1,35 @@
 import express from "express";
-import { createUser  , getAllUsers, loginUser , logOutCurrectUser , getCurrentUserProfile , updateCurrentUserProfile , deleteUserById , getUserById , updateUserById} from "../controllers/user.controller.js";
+import {
+  createUser,
+  getAllUsers,
+  loginUser,
+  logOutCurrectUser,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+  deleteUserById,
+  getUserById,
+  updateUserById,
+} from "../controllers/user.controller.js";
 const router = express();
-import { authenticate , authorizeAdmin } from "../middlewares/auth.middleware.js";
-router.route('/').post(createUser).get(authenticate , authorizeAdmin , getAllUsers);
-router.post('/auth' , loginUser);
-router.post('/logout' , logOutCurrectUser);
-router.route('/profile').get(authenticate , getCurrentUserProfile).put(authenticate , updateCurrentUserProfile);
+import {
+  authenticate,
+  authorizeAdmin,
+} from "../middlewares/auth.middleware.js";
+router
+  .route("/")
+  .post(createUser)
+  .get(authenticate, authorizeAdmin, getAllUsers);
+  // http://localhost:5000/api/users/auth
+router.post("/auth", loginUser);
+router.post("/logout", logOutCurrectUser);
+router
+  .route("/profile")
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate, updateCurrentUserProfile);
 // admin route
-router.route('/:id').delete(authenticate , authorizeAdmin , deleteUserById).get(authenticate , authorizeAdmin , getUserById).put(authenticate , authorizeAdmin , updateUserById);
+router
+  .route("/:id")
+  .delete(authenticate, authorizeAdmin, deleteUserById)
+  .get(authenticate, authorizeAdmin, getUserById)
+  .put(authenticate, authorizeAdmin, updateUserById);
 export default router;
