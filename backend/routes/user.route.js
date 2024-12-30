c34fcc86de734f099796577ff70fc16292e181b6
iDinesh04 <dineshkumarsahoo.2025@gmail.com>
2024-12-30 19:12:33 +0530

    deletion is done

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createUser  , getAllUsers, loginUser , logOutCurrectUser , getCurrentUserProfile} from "../controllers/user.controller.js";
+import { createUser  , getAllUsers, loginUser , logOutCurrectUser , getCurrentUserProfile , updateCurrentUserProfile , deleteUserById} from "../controllers/user.controller.js";
 const router = express();
 import { authenticate , authorizeAdmin } from "../middlewares/auth.middleware.js";
 router.route('/').post(createUser).get(authenticate , authorizeAdmin , getAllUsers);
 router.post('/auth' , loginUser);
 router.post('/logout' , logOutCurrectUser);
-router.route('/profile').get(authenticate , getCurrentUserProfile);
+router.route('/profile').get(authenticate , getCurrentUserProfile).put(authenticate , updateCurrentUserProfile);
+// admin route
+router.route('/:id').delete(authenticate , authorizeAdmin , deleteUserById);
 export default router;
