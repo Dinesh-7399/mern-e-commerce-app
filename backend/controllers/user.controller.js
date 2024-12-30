4524594431923cf86b1854a235971e2fdf1b3eaf
iDinesh04 <dineshkumarsahoo.2025@gmail.com>
2024-12-30 13:19:22 +0530

    encrypted the password and created the cookie for the user

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,20 +1,27 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
-// import user from "../models/user.model.js";
 import User from "../models/user.model.js";
-
+import bcrypt from "bcryptjs";
+import createToken from '../utils/createToken.utils.js';
 const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
-  console.log(`${username} , ${email} , ${password}`);
+
   if (!username || !password || !email) {
     throw new Error("Please fill all the fields");
   }
+
   const userExists = await User.findOne({ email });
   if (userExists) {
     res.status(400).send("User already exists");
   }
-  const newUser = new User({ username, email, password });
+
+  // will add salt to the password i.e some random chars in between the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
+    createToken(res , newUser._id);
     res.status(201).json({
       _id: newUser._id,
       username: newUser.username,
