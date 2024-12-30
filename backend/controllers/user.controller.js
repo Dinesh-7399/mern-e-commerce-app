af5228d7405f1d127a5421697003d3534f545974
iDinesh04 <dineshkumarsahoo.2025@gmail.com>
2024-12-30 13:00:09 +0530

    user created

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,10 +1,30 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
+// import user from "../models/user.model.js";
 import User from "../models/user.model.js";
 
-const createUser  = asyncHandler(async(req , res) => {
-  const {username , email , password} = req.body;
+const createUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
   console.log(`${username} , ${email} , ${password}`);
-  res.send('OK');
+  if (!username || !password || !email) {
+    throw new Error("Please fill all the fields");
+  }
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400).send("User already exists");
+  }
+  const newUser = new User({ username, email, password });
+  try {
+    await newUser.save();
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 export default createUser;
