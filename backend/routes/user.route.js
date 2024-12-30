07ab32e1bafb3af1f1dcee211de3428297a8eb7c
iDinesh04 <dineshkumarsahoo.2025@gmail.com>
2024-12-30 19:28:03 +0530

    update delete and access user data as admin

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser  , getAllUsers, loginUser , logOutCurrectUser , getCurrentUserProfile , updateCurrentUserProfile , deleteUserById} from "../controllers/user.controller.js";
+import { createUser  , getAllUsers, loginUser , logOutCurrectUser , getCurrentUserProfile , updateCurrentUserProfile , deleteUserById , getUserById , updateUserById} from "../controllers/user.controller.js";
 const router = express();
 import { authenticate , authorizeAdmin } from "../middlewares/auth.middleware.js";
 router.route('/').post(createUser).get(authenticate , authorizeAdmin , getAllUsers);
@@ -7,5 +7,5 @@ router.post('/auth' , loginUser);
 router.post('/logout' , logOutCurrectUser);
 router.route('/profile').get(authenticate , getCurrentUserProfile).put(authenticate , updateCurrentUserProfile);
 // admin route
-router.route('/:id').delete(authenticate , authorizeAdmin , deleteUserById);
+router.route('/:id').delete(authenticate , authorizeAdmin , deleteUserById).get(authenticate , authorizeAdmin , getUserById).put(authenticate , authorizeAdmin , updateUserById);
 export default router;
